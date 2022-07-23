d12f822f7b030bc186abdc0070133d1ced5c9e72
Matteo Serafino <matteo_serafino@hotmail.com>
2022-07-23 23:32:38 +0200

    Edit the target column name from label to target

--- dimensionality_reduction/services/plot_service.py
+++ dimensionality_reduction/services/plot_service.py
@@ -6,18 +6,20 @@ TITLE_FONT_SIZE = 16
 LABEL_FONT_SIZE = 12
 FIGURE_SIZE = 10
 
-def plot_2d(df: DataFrame, target_column: str = "label", title: str = None) -> None:
+def plot_2d(df: DataFrame, target_column: str = "target", title: str = None) -> None:
 
     plt.figure(figsize=((FIGURE_SIZE, FIGURE_SIZE)))
 
     categories = df[target_column].unique()
 
     for category in categories:
-        plt.scatter(df.loc[df[target_column] == category, df.columns[0]],
+        plt.scatter(
+            df.loc[df[target_column] == category, df.columns[0]],
             df.loc[df[target_column] == category, df.columns[1]],
             marker = 'o',
             alpha = 0.5,
-            label=category)
+            label=category
+        )
     
     plt.legend()
 
@@ -29,7 +31,7 @@ def plot_2d(df: DataFrame, target_column: str = "label", title: str = None) -> N
 
     return None
 
-def plot_3d(df: DataFrame, target_column: str = "label", title: str = None) -> None:
+def plot_3d(df: DataFrame, target_column: str = "target", title: str = None) -> None:
 
     fig = plt.figure(figsize = (FIGURE_SIZE, FIGURE_SIZE))
     chart = fig.gca(projection = '3d')
@@ -37,12 +39,14 @@ def plot_3d(df: DataFrame, target_column: str = "label", title: str = None) -> N
     categories = df[target_column].unique()
 
     for category in categories:
-        chart.scatter(df.loc[df[target_column] == category, df.columns[0]],
+        chart.scatter(
+            df.loc[df[target_column] == category, df.columns[0]],
             df.loc[df[target_column] == category, df.columns[1]], 
             df.loc[df[target_column] == category, df.columns[2]],
             marker = 'o',
             alpha = 0.5, 
-            label=category)
+            label=category
+        )
 
     chart.legend()
 
@@ -53,7 +57,7 @@ def plot_3d(df: DataFrame, target_column: str = "label", title: str = None) -> N
 
     return None
 
-def matrix_plot(df: DataFrame, target_column: str = "label", title: str = None) -> None:
+def matrix_plot(df: DataFrame, target_column: str = "target", title: str = None) -> None:
 
     pairplot(df, hue=target_column, height=2.5, palette="tab10")
     plt.grid(b=True)
